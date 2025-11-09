8135fce2d383c54153432619ddffbc9ff2a59fde
steele.baldock@gmail.com <steele.baldock@gmail.com>
2025-11-09 11:59:07 +1100

    added more code

--- main.py
+++ main.py
@@ -10,14 +10,27 @@ import sqlite3
 # Setup filename for database and connect sqlite3 to it.
 
 conn = sqlite3.connect('steeletodo.db')
+
 # Check if the database already exists and has data in it. If it is empty or does not exist, set it up and add something to it.
 conn.execute('''CREATE TABLE IF NOT EXISTS todo(id INTEGER PRIMARY KEY, task TEXT NOT NULL);''')
 
 # Define our Global Functions
 # Show our todo List
-def show()
+def show():
     query = "SELECT * FROM todo;"
     conn.execute(query,(task,))
     return conn.execute(query)
-def insertdata(task)
+
+#Add a new item to the todo list
+def insertdata(task):
+    query = "INSERT INTO todo(task) VALUES(?);"
+    conn.execute(query, (task,))
+    conn.commit()
+
+#Delete a task from the todo List
+def deletebytask(taskval):
+    query = "DELETE FROM todo WHERE task =?;"
+    conn.execute(query, (taskval,))
+    conn.commit()
+
 
